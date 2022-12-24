224beeb7c440a2db43022b873bb65222e40bd36f
TagCopperLight <tagcopperlight@gmail.com>
2022-12-24 17:53:10 +0100

    NSV Ode Solver

---
+++ pyphysics/solvers/nsv_ode_solver.py
@@ -0,0 +1,27 @@
+from ..system_state import SystemState
+from .ode_solver import OdeSolver
+
+
+class NSVOdeSolver(OdeSolver):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def start(self, system_state_initial: SystemState, dt: float) -> None:
+        self.dt = dt
+
+    def step(self, system_state: SystemState) -> bool:
+        return False
+    
+    def solve(self, system_state: SystemState) -> None:
+        for i in range(len(system_state.bodies)):
+            system_state.accelerations[i] = system_state.forces[i] / system_state.masses[i]
+            system_state.angular_accelerations[i] = system_state.torques[i] / system_state.inertias[i]
+
+            system_state.velocities[i] += system_state.accelerations[i] * self.dt
+            system_state.angular_velocities[i] += system_state.angular_accelerations[i] * self.dt
+            
+            system_state.positions[i] += system_state.velocities[i] * self.dt
+            system_state.angles[i] += system_state.angular_velocities[i] * self.dt
+
+    def end(self):
+        pass
