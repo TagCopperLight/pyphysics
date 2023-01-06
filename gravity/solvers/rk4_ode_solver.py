2d77353a87c915978f53fb3d070c844bf0c29dd7
TagCopperLight <tagcopperlight@gmail.com>
2023-01-06 17:00:36 +0100

    Added RK4 ode solver

--- gravity/solvers/rk4_ode_solver.py
+++ gravity/solvers/rk4_ode_solver.py
@@ -1,27 +1,55 @@
 from ..system_state import SystemState
 from .ode_solver import OdeSolver
 
+from copy import deepcopy
+
 
 class Rk4OdeSolver(OdeSolver):
     def __init__(self) -> None:
         super().__init__()
+        self.stage = 1
+        self.next_stage = 1
     
     def start(self, system_state_initial: SystemState, dt: float) -> None:
         self.dt = dt
+        self.initial_state = deepcopy(system_state_initial)
+        self.accumulator = deepcopy(system_state_initial)
 
     def step(self, system_state: SystemState) -> bool:
-        return False
+        if self.stage == 2 or self.stage == 3:
+            for i in range(len(system_state.bodies)):
+                system_state.angular_velocities[i] = self.initial_state.angular_velocities[i] + self.dt * system_state.angular_accelerations[i] / 2
+                system_state.angles[i] = self.initial_state.angles[i] + self.dt * system_state.angular_velocities[i] / 2
+                system_state.velocities[i] = self.initial_state.velocities[i] + self.dt * system_state.accelerations[i] / 2
+                system_state.positions[i] = self.initial_state.positions[i] + self.dt * system_state.velocities[i] / 2
+
+        elif self.stage == 4:
+            for i in range(len(system_state.bodies)):
+                system_state.angular_velocities[i] = self.initial_state.angular_velocities[i] + self.dt * system_state.angular_accelerations[i]
+                system_state.angles[i] = self.initial_state.angles[i] + self.dt * system_state.angular_velocities[i]
+                system_state.velocities[i] = self.initial_state.velocities[i] + self.dt * system_state.accelerations[i]
+                system_state.positions[i] = self.initial_state.positions[i] + self.dt * system_state.velocities[i]
+
+        self.next_stage += 1
+
+        return self.next_stage > 4
     
     def solve(self, system_state: SystemState) -> None:
-        for i in range(len(system_state.bodies)):
-            system_state.accelerations[i] = system_state.forces[i] / system_state.masses[i]
-            system_state.angular_accelerations[i] = system_state.torques[i] / system_state.inertias[i]
-            
-            system_state.positions[i] += system_state.velocities[i] * self.dt
-            system_state.angles[i] += system_state.angular_velocities[i] * self.dt
+        stage_weight = {1:1, 2:2, 3:2, 4:1}[self.stage]
 
-            system_state.velocities[i] += system_state.accelerations[i] * self.dt
-            system_state.angular_velocities[i] += system_state.angular_accelerations[i] * self.dt
+        for i in range(len(system_state.bodies)):
+            self.accumulator.angular_velocities[i] += (self.dt / 6) * stage_weight * system_state.angular_accelerations[i]
+            self.accumulator.angles[i] += (self.dt / 6) * stage_weight * system_state.angular_velocities[i]
+            self.accumulator.velocities[i] += (self.dt / 6) * stage_weight * system_state.accelerations[i]
+            self.accumulator.positions[i] += (self.dt / 6) * stage_weight * system_state.velocities[i]
+        
+        if self.stage == 4:
+            system_state.angular_velocities = self.accumulator.angular_velocities
+            system_state.angles = self.accumulator.angles
+            system_state.velocities = self.accumulator.velocities
+            system_state.positions = self.accumulator.positions
+        
+        self.stage = self.next_stage
 
     def end(self) -> None:
-        pass
+        self.stage = -1
